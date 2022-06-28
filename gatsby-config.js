99ccd54cbbef1124282fd4317f50682bcc61913c
Al Nelson <hi@mynameis.al>
2022-06-28 10:41:04 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `wall_Flower_Full`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"]
+};
