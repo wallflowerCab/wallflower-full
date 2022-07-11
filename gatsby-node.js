e908bde39bfbebf26f223df23ad2d8115eea32dd
Al Nelson <hi@mynameis.al>
2022-07-11 10:04:31 -0700

    index query

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,13 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.(glb)$/i,
-          use: [
-            {
-              loader: "url-loader",
-            },
-          ],
-        },
-      ],
-    },
-  })
-}
+
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
   return graphql(`
-      {
+    query IndexQuery  {
         allDatoCmsProject {
           edges {
             node {
