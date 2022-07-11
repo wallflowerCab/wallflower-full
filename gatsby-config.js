98b8e9936f0d0e8924efe81f78be9a400193c130
Al Nelson <hi@mynameis.al>
2022-07-11 11:07:39 -0700

    remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,17 @@ module.exports = {
     },
     __key: "images"
   },
+  {
+   resolve: `gatsby-transformer-remark`,
+   options: {
+     // Footnotes mode (default: true)
+     footnotes: true,
+     // GitHub Flavored Markdown mode (default: true)
+     gfm: true,
+     // Plugins configs
+     plugins: [],
+   },
+ },
 
    {
       resolve: 'gatsby-plugin-root-import',
